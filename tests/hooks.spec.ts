353acc4ab6666433ad64ac3ba4198bf0531a1010
07akioni <07akioni2@gmail.com>
2022-04-03 17:28:43 +0800

    test: hooks test

--- tests/hooks.spec.ts
+++ tests/hooks.spec.ts
@@ -6,6 +6,42 @@ beforeEach(test)
 
 test('`hooks` should work', async ({ page }) => {
   const body = await page.evaluate(async () => {
+    return (
+      await window.lyla.post('/gigigi', {
+        hooks: {
+          onBeforeOptionsNormalized: [
+            (options) => {
+              if (options.url === '/gigigi') {
+                options.url = '/api/post-return-body'
+              }
+              return options
+            }
+          ],
+          onBeforeRequest: [
+            (options) => {
+              options.json = { foo: 'bar' }
+              return options
+            }
+          ],
+          onAfterResponse: [
+            (resp) => {
+              if (
+                '{"foo":"bar"}' === (resp.body as string).replace(/\s\n/g, '')
+              ) {
+                resp.body = 'jojo'
+              }
+              return resp
+            }
+          ]
+        }
+      })
+    ).body
+  })
+  expect(body).toEqual('jojo')
+})
+
+test('`hooks` should work with `extend`', async ({ page }) => {
+  const body = await page.evaluate(async () => {
     const lyla = window.lyla.extend({
       hooks: {
         onBeforeOptionsNormalized: [
