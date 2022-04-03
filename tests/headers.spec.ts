94bccb78fdff5a282618358213a52cc1cccbb5f1
07akioni <07akioni2@gmail.com>
2022-04-03 17:40:39 +0800

    test: headers override

---
+++ tests/headers.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test'
+import { beforeEach } from './utils'
+import './types'
+
+beforeEach(test)
+
+test('`headers` can be merged with different case', async ({ page }) => {
+  const headers = await page.evaluate(async () => {
+    const lyla = window.lyla.extend({
+      headers: {
+        Key: 'Key'
+      }
+    })
+    const { headers } = await lyla.post('/api/post-return-headers', {
+      headers: {
+        key: 'key'
+      }
+    })
+    return headers
+  })
+  expect(headers.key).toEqual('key')
+})
