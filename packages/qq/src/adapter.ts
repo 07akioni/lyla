753131618dd78e642c0ad6822a599a1085f95e33
07akioni <07akioni2@gmail.com>
2022-06-16 21:26:04 +0800

    feat: qq miniprogram

---
+++ packages/qq/src/adapter.ts
@@ -0,0 +1,69 @@
+import type {
+  LylaAdapter,
+  LylaAdapterMeta as LylaCoreAdapterMeta,
+} from '@lylajs/core'
+import type {
+  NetworkErrorDetail,
+  ResponseDetail,
+  QqRequest,
+  QqRequestMethod
+} from './types.js'
+
+declare const tt: {
+  request: QqRequest
+}
+
+export interface LylaAdapterMeta extends LylaCoreAdapterMeta {
+  method: QqRequestMethod
+  networkErrorDetail: NetworkErrorDetail
+  responseDetail: ResponseDetail
+  responseType: 'arraybuffer' | 'text'
+  requestBody: string | ArrayBuffer
+}
+
+export const adapter: LylaAdapter<LylaAdapterMeta> = ({
+  url,
+  method,
+  headers,
+  body,
+  responseType,
+  onResponse,
+  onNetworkError
+  // Not used, just leave it here
+  // json,
+  // withCredentials,
+  // onDownloadProgress,
+  // onUploadProgress,
+}): {
+  abort: () => void
+} => {
+  const requestTask = tt.request({
+    url,
+    method,
+    header: headers,
+    data: body,
+    responseType,
+    // https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+    // Docs said if it's not json, response data won't be transformed to json.
+    dataType: 'text',
+    fail(res) {
+      onNetworkError(res)
+    },
+    success(res) {
+      onResponse(
+        {
+          body: res.data as string | ArrayBuffer,
+          status: res.statusCode,
+          statusText: `${res.statusCode}`,
+          headers: res.header
+        },
+        res
+      )
+    }
+  })
+  return {
+    abort() {
+      requestTask.abort()
+    }
+  }
+}
---
+++ packages/qq/src/types.ts
@@ -0,0 +1,41 @@
+export type ResponseDetail = {
+  statusCode: number
+  header: Record<string, string>
+  data: string | object | ArrayBuffer
+}
+
+export type NetworkErrorDetail = unknown
+
+export type QqRequestMethod =
+  | 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'OPTIONS'
+  | 'HEAD'
+  | 'DELETE'
+  | 'TRACE'
+  | 'CONNECT'
+  | 'get'
+  | 'post'
+  | 'put'
+  | 'options'
+  | 'head'
+  | 'delete'
+  | 'trace'
+  | 'connect'
+
+export type QqRequestOptions = {
+  url: string
+  header?: Record<string, string>
+  method?: QqRequestMethod
+  data?: string | object | ArrayBuffer
+  dataType?: 'json' | string
+  responseType?: 'text' | 'arraybuffer'
+  success?: (res: ResponseDetail) => void
+  fail?: (res: NetworkErrorDetail) => void
+  complete?: (res: { errMsg: string }) => void
+}
+
+export type QqRequestTask = { abort: () => void }
+
+export type QqRequest = (options: QqRequestOptions) => QqRequestTask
